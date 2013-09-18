870ebb6c365bb15428daf380ecd522a1c0e830a8
Ziptrek Administrator <administrator@ubuntu.test.local>
2013-09-18 15:08:00 -0700

    add interactive mode

--- network.sh
+++ network.sh
@@ -1,30 +1,62 @@
 #!/bin/bash
-#Script to configure Ubuntu network interface
-
-IFNAME=eth1
-IFTYPE="static"
-IFADDRESS="10.0.1.1"
+# Script to configure Ubuntu network interface
+# Copyright 2013, Andrew Bythell <abythell@ieee.org>
+#
+# Script may be run with no arguments in interactive mode, or
+# by specifying command line arguments:
+#
+#	network.sh <ifname> dhcp
+# or
+#	network.sh <ifname> static <ip-address> 
+# where
+#	ifname=eth1, eth2, etc.
 
 #
-# check for permissions
+# check for permissions - must run as root
 #
 if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root/sudo user"
 	exit 1
 fi 
 
-echo "Current network interfaces:"
-cat /etc/network/interfaces | grep iface | awk '{print $2}'
-echo 
-echo "Name of interface to configure? (eg. 'eth1')"
-read IFNAME
-echo "Network type: static or dhcp?"
-read IFTYPE
+#
+# check for command line arguments
+#
+if [ $# -ne 0 ]
+then
+	IFNAME=$1
+	IFTYPE=$2
+	if [ "$IFTYPE" == "static" ]
+	then
+		if [ $# -ne 3 ]
+		then
+			echo "usage: network.sh <ifname> <static|dhcp> <ip addr>"
+			exit -1
+		fi
+		IFADDRESS=$3
+	elif [ "$IFTYPE" != "dhcp" ]
+	then
+		echo "type must be 'static' or 'dhcp'"
+		exit -1
+	fi 
+else
+	echo "Current network interfaces:"
+	cat /etc/network/interfaces | grep iface | awk '{print $2}'
+	echo 
+	echo "Name of interface to configure? (eg. 'eth1')"
+	read IFNAME
+	echo "Network type: static or dhcp?"
+	read IFTYPE
+	if [ "$IFTYPE" == "static" ]
+	then
+		echo "IP Address?"
+		read IFADDRESS
+	fi
+fi
+
 
 if [ "$IFTYPE" == "static" ]
 then
-	echo "IP Address?"
-	read IFADDRESS
 	cat >> /etc/network/interfaces << EOL
 
 auto $IFNAME
@@ -32,6 +64,7 @@ iface $IFNAME inet static
 address $IFADDRESS 
 netmask 255.255.255.0
 EOL
+	### end interfaces
 
 elif [ "$IFTYPE" == "dhcp" ]
 then
@@ -40,6 +73,7 @@ then
 auto $IFNAME
 iface $IFNAME inet dhcp
 EOL
-
+	## end interfaces
 fi
+
 ifup $IFNAME
