93e4c3807371ddcfa330a25fd8171eeffda8b251
Administrator <administrator@sr-node1.(none)>
2013-09-25 15:42:09 -0700

    make drbd-specific (remove iscsi stuff)

--- drbd.sh
+++ drbd.sh
@@ -11,128 +11,30 @@ if [[ $EUID -ne 0 ]]; then
 fi 
 
 #
-# check number of disks
+# Disk Settings
 #
-DISK_COUNT=`fdisk -l | grep "^Disk /dev" | wc -l`
-if [ "$DISK_COUNT" -lt 4 ]
-then
-	echo "This node needs at least 4 disks/logical volumes."  
-	exit -1
-fi
-
-#
-# check number of interfaces
-#
-IF_COUNT=`wc -w <<<$(netstat -i | cut -d" " -f1 | egrep -v "^Kernel|Iface|lo")`
-if [ "$IF_COUNT" -lt 2 ]
-then
-	echo "This node needs at least two network intefaces."
-	exit -1
-fi
-
-#
-# User Settings (TODO: read values from user)
-#
-echo "DRBD Meta Device (eg. /dev/xvdb)"
+echo "DRBD Meta Device (eg. /dev/sdb1, /dev/xvdb1)"
 read DEV_DRBD
-echo "iSCSI Config Device (eg. /dev/xvdc)"
-read DEV_CONFIG
-echo "iSCSI LUN Device (eg. /dev/xvde)"
+echo "iSCSI LUN Device (eg. /dev/sdb1, /dev/xvdc1)"
 read DEV_ISCSI
+echo "Primary Node Name (on SAN)"
+read PRIMARY_HOST
+echo "Secondary Node Name (on SAN)"
+read SECONDARY_HOST
+echo "Is this the primary node? (y/n)"
+read IS_PRIMARY
 
-LOCAL_FQDN=`hostname --fqdn`
-LOCAL_HOST=`hostname`
-LOCAL_LAN=`ip addr show eth0 | grep "inet " | awk '{print $2}' | sed 's/\/.*//'`
-LOCAL_SAN=`ip addr show eth1 | grep "inet " | awk '{print $2}' | sed 's/\/.*//'`
-
-echo "Is this Primary or Secondary node (1/2)?"
-read primary
-if [ "$primary" == "1" ] 
-then
-	IS_PRIMARY=true
-	PRIMARY_FQDN=$LOCAL_FQDN
-	PRIMARY_HOST=$LOCAL_HOST
-	PRIMARY_LAN=$LOCAL_LAN
-	PRIMARY_SAN=$LOCAL_SAN
-	
-	echo "Primary Node (localhost) info:"
-	echo "	$PRIMARY_FQDN"
-	echo "	$PRIMARY_HOST"
-	echo "	$PRIMARY_LAN"
-	echo "	$PRIMARY_SAN"
-	echo 
-
-	echo "FQDN of Secondary node"
-	read SECONDARY_FQDN
-	SECONDARY_HOST="${SECONDARY_FQDN%%.*}"
-	echo "Secondary node LAN address (managment, iscsi)"
-	read SECONDARY_LAN
-	echo "Secondary node SAN address (replication, heartbeat)"
-	read SECONDARY_SAN
-else
-	SECONDARY_FQDN=$LOCAL_FQDN
-	SECONDARY_HOST=$LOCAL_HOST
-	SECONDARY_LAN=$LOCAL_LAN
-	SECONDARY_SAN=$LOCAL_SAN
-
-	echo "Secondary Node (localhost) info:"
-	echo "	$SECONDARY_FQDN"
-	echo "	$SECONDARY_HOST"
-	echo "	$SECONDARY_LAN"
-	echo "	$SECONDARY_SAN"
-	echo
-
-	IS_PRIMARY=false
-	echo "FQDN of Primary node"
-	read PRIMARY_FQDN
-	PRIMARY_HOST="${PRIMARY_FQDN%%.*}"
-	echo "Primary node LAN address (managment, iscsi)"
-	read PRIMARY_LAN
-	echo "Primary node SAN address (replication, heartbeat)"
-	read PRIMARY_SAN
-
-fi 
-
-echo "What is the Virtual IP of the iSCSI target?"
-read VIRTUAL_LAN
-
-
-#
-# Advanced Users Settings (don't need to modify in most situations)
-#
-IQN=iqn.2013-09.com.ziptrek:iscsi.0.`date +%s`
-PART_DRBD="$DEV_DRBD"1
-PART_CONFIG="$DEV_CONFIG"1
-PART_ISCSI="$DEV_ISCSI"1
-
-#
-# Partition.  
-# Three paritions are required:  one for the DRBD metadata,
-# one for config files, and one for the iSCSI target
-#
-echo "Partioning."
-for disk in $DEV_DRBD $DEV_CONFIG $DEV_ISCSI
-do
-	dd if=/dev/zero of=$disk bs=512 count=1
-	(echo n; echo p; echo 1; echo; echo; echo w) | fdisk $disk
-done
-
-#
-# Configure Hosts
-#
-#echo "Updating /etc/hosts"
-#echo "$PRIMARY_LAN	$PRIMARY_FQDN	${PRIMARY_HOST}" >> /etc/hosts
-#echo "$SECONDARY_LAN	$SECONDARY_FQDN	${SECONDARY_HOST}" >> /etc/hosts
+PRIMARY_IP=`getent ahosts $PRIMARY_HOST | grep STREAM | awk '{print $1}'`
+SECONDARY_IP=`getent ahosts $SECONDARY_HOST | grep STREAM | awk '{print $1}'`
 
 #
 # Install some packages
 #
-apt-get -y install ntp drbd8-utils iscsitarget iscsitarget-dkms jfsutils
+apt-get -y install ntp drbd8-utils
 
 #
 # Configure DRBD
 #
-echo "Configuring DRBD"
 for file in /sbin/drbdsetup /sbin/drbdadm /sbin/drbdmeta
 do
 	chgrp haclient $file
@@ -140,157 +42,47 @@ do
 	chmod u+s $file
 done
 
-### begin iscsi.res ###
-cat > /etc/drbd.d/iscsi.res << EOL
-resource iscsi.config {
+### begin iscsi-lun1.res ###
+cat > /etc/drbd.d/iscsi-lun1.res << EOL
+resource iscsi.lun.1 {
         protocol A;
  
-        handlers {
-        pri-on-incon-degr "echo o > /proc/sysrq-trigger ; halt -f";
-        pri-lost-after-sb "echo o > /proc/sysrq-trigger ; halt -f";
-        local-io-error "echo o > /proc/sysrq-trigger ; halt -f";
-        outdate-peer "/usr/lib/heartbeat/drbd-peer-outdater -t 5";      
-        }
-
-        startup {
-        degr-wfc-timeout 120;
-        }
-
         disk {
-        on-io-error detach;
+          on-io-error detach;
+	  fencing resource-only;
         }
 
         net {
-        cram-hmac-alg sha1;
-        shared-secret "password";
-        after-sb-0pri disconnect;
-        after-sb-1pri disconnect;
-        after-sb-2pri disconnect;
-        rr-conflict disconnect;
+          cram-hmac-alg sha1;
+          shared-secret "password";
         }
 
         syncer {
         rate 100M;
-        verify-alg sha1;
         al-extents 257;
         }
 
+        device  /dev/drbd1;
+        disk    $DEV_ISCSI;
+        meta-disk $DEV_DRBD[1];
+
         on $PRIMARY_HOST {
-        device  /dev/drbd0;
-        disk    $PART_CONFIG;
-        address $PRIMARY_SAN:7788;
-        meta-disk $PART_DRBD[0];
+          address $PRIMARY_IP:7789;
         }
 
         on $SECONDARY_HOST {
-        device  /dev/drbd0;
-        disk    $PART_CONFIG;
-        address $SECONDARY_SAN:7788;
-        meta-disk $PART_DRBD[0];
-        }
-}
-
-resource iscsi.target.0 {
-        protocol A;
- 
-        handlers {
-        pri-on-incon-degr "echo o > /proc/sysrq-trigger ; halt -f";
-        pri-lost-after-sb "echo o > /proc/sysrq-trigger ; halt -f";
-        local-io-error "echo o > /proc/sysrq-trigger ; halt -f";
-        outdate-peer "/usr/lib/heartbeat/drbd-peer-outdater -t 5";      
-        }
-
-        startup {
-        degr-wfc-timeout 120;
-        }
-
-        disk {
-        on-io-error detach;
-        }
-
-        net {
-        cram-hmac-alg sha1;
-        shared-secret "password";
-        after-sb-0pri disconnect;
-        after-sb-1pri disconnect;
-        after-sb-2pri disconnect;
-        rr-conflict disconnect;
-        }
-
-        syncer {
-        rate 100M;
-        verify-alg sha1;
-        al-extents 257;
-        }
-
-        on $PRIMARY_HOST {
-        device  /dev/drbd1;
-        disk    $PART_ISCSI;
-        address $PRIMARY_SAN:7789;
-        meta-disk $PART_DRBD[1];
-        }
-
-        on $SECONDARY_HOST {
-        device  /dev/drbd1;
-        disk    $PART_ISCSI;
-        address $SECONDARY_SAN:7789;
-        meta-disk $PART_DRBD[1];
+          address $SECONDARY_IP:7789;
         }
 }
 EOL
-### end iscsi.res
+### end iscsi-lun1.res
 
 #
 # Initialize DRBD 
 #
-echo "Initializing DRBD disks"
-mkdir -p /mnt/config
-drbdadm create-md all
+drbdadm create-md iscsi.lun.1
 service drbd restart
-if [ "$IS_PRIMARY" == "true" ]
+if [ "$IS_PRIMARY" == "y" ]
 then
-	echo "Configuring primary node"
-	drbdadm -- --overwrite-data-of-peer primary all
-	mkfs.jfs /dev/drbd0
-	mount /dev/drbd0 /mnt/config
-	mkdir -p /mnt/config/iet
-
-	### begin ietd.conf
-	cat > /mnt/config/iet/ietd.conf << EOL	
-	Target $IQN 
-        #IncomingUser geekshlby secret
-        #OutgoingUser geekshlby password
-        Lun 0 Path=/dev/drbd1,Type=blockio
-        Alias disk0
-        MaxConnections         1
-        InitialR2T             Yes
-        ImmediateData          No
-        MaxRecvDataSegmentLength 8192
-        MaxXmitDataSegmentLength 8192
-        MaxBurstLength         262144
-        FirstBurstLength       65536
-        DefaultTime2Wait       2
-        DefaultTime2Retain     20
-        MaxOutstandingR2T      8
-        DataPDUInOrder         Yes
-        DataSequenceInOrder    Yes
-        ErrorRecoveryLevel     0
-        HeaderDigest           CRC32C,None
-        DataDigest             CRC32C,None
-        Wthreads               8
-EOL
-	### end ietd.conf
-
-	echo ALL ALL > /mnt/config/iet/initiators.allow	
-	echo ALL $VIRTUAL_LAN > /mnt/config/iet/targets.allow
-	umount /mnt/config
+	drbdadm -- --overwrite-data-of-peer primary iscsi.lun.1
 fi
-
-#
-# Setup iscsi target
-#
-sed -i s/false/true/ /etc/default/iscsitarget
-update-rc.d -f iscsitarget remove
-mv /etc/iet /etc/iet.orig
-ln -s /mnt/config/iet /etc/iet
-
